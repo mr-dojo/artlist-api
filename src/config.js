1236f07a4e03c9c1547cbf24988b617275b852a2
Carlo Costantino <costantinoart@gmail.com>
2020-02-28 14:25:57 -1000

    Added dynamic CLIENT_ORIGIN to config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 module.exports = {
   NODE_ENV: process.env.NODE_ENV || "development",
-  PORT: process.env.PORT || 8000
+  PORT: process.env.PORT || 8000,
+  API_BASE_URL: process.env.REACT_APP_API_BASE_URL || "http://localhost:3000",
+  CLIENT_ORIGIN: process.env.REACT_APP_CLIENT_ORIGIN || "http://localhost:3000"
 };
