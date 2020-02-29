80f8edb9a21cf85d266fd8e317f2bcf252529b1b
Carlo Costantino <costantinoart@gmail.com>
2020-02-28 14:27:13 -1000

    Added dummy data for server

---
+++ src/store.js
@@ -0,0 +1,42 @@
+const allEntries = [
+  {
+    title: "Lahaina Fish",
+    medium: "oil",
+    description: "a colorful green and purple fish",
+    location: "Maui Hands Lahaina",
+    availability: "Complicated",
+    price: "2500"
+  },
+  {
+    title: "Honu",
+    medium: "mixed media",
+    description: "a turtle in a sea of blue",
+    availability: "Unavailable",
+    location: "somewhere in Canada"
+  },
+  {
+    title: "Dancing Clown",
+    medium: "acrylic",
+    description: "a scary clown",
+    location: "Maui Hands Lahaina",
+    availability: "Complicated",
+    price: "2500"
+  },
+  {
+    title: "Makawao Fish",
+    medium: "mixed media",
+    description: "A fish of the mountain",
+    availability: "Unavailable",
+    size: "20x40"
+  },
+  {
+    title: "Haleakala",
+    medium: "acrylic",
+    description: "A giant volcano that looks like a mountain",
+    size: "20x16",
+    availability: "Available",
+    price: "3000"
+  }
+];
+
+module.exports = allEntries;
