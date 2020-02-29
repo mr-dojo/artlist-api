441e530f1916fd3f5fa9d7cb6621cc1078456f65
Carlo Costantino <costantinoart@gmail.com>
2020-02-28 18:00:46 -1000

    Added knex db connection, passed it to app.js, and added DATABASE_URL to config.js

--- src/config.js
+++ src/config.js
@@ -2,5 +2,7 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || "development",
   PORT: process.env.PORT || 8000,
   API_BASE_URL: process.env.REACT_APP_API_BASE_URL || "http://localhost:3000",
-  CLIENT_ORIGIN: process.env.REACT_APP_CLIENT_ORIGIN || "http://localhost:3000"
+  CLIENT_ORIGIN: process.env.REACT_APP_CLIENT_ORIGIN || "http://localhost:3000",
+  DATABASE_URL:
+    process.env.DATABASE_URL || "postgresql://dunder_mifflin@localhost/artlist"
 };
--- src/server.js
+++ src/server.js
@@ -1,5 +1,13 @@
+const knex = require("knex");
 const app = require("./app");
-const { PORT } = require("./config");
+const { PORT, DATABASE_URL } = require("./config");
+
+const db = knex({
+  client: "pg",
+  connection: DATABASE_URL
+});
+
+app.set("db", db);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port http://localhost:${PORT}`);
