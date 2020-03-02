a17c4d1b36dc9fd159f545dfdd4fce4d20766974
Carlo Costantino <costantinoart@gmail.com>
2020-03-02 11:56:35 -1000

    Added POST endpoint and servie function

--- src/list/list-router.js
+++ src/list/list-router.js
@@ -3,13 +3,48 @@ const allEntries = require("../store");
 const ListService = require("./list-service");
 
 const listRouter = express.Router();
+const jsonParser = express.json();
 
-listRouter.route("/").get((req, res, next) => {
-  ListService.getList(req.app.get("db"))
-    .then(list => {
-      res.status(200).json(list);
-    })
-    .catch(next);
-});
+listRouter
+  .route("/")
+  .get((req, res, next) => {
+    ListService.getList(req.app.get("db"))
+      .then(list => {
+        res.status(200).json(list);
+      })
+      .catch(next);
+  })
+  .post(jsonParser, (req, res, next) => {
+    const {
+      title,
+      description,
+      medium,
+      location,
+      price,
+      size,
+      availability
+    } = req.body;
+    const newItem = {
+      title,
+      description,
+      medium,
+      location,
+      price,
+      size,
+      availability
+    };
+
+    if (!newItem.title) {
+      return res.status(400).json({
+        error: { message: `Missing 'title' in request body` }
+      });
+    }
+
+    ListService.insertItem(req.app.get("db"), newItem)
+      .then(note => {
+        res.status(201).json(note);
+      })
+      .catch(next);
+  });
 
 module.exports = listRouter;
--- src/list/list-service.js
+++ src/list/list-service.js
@@ -1,6 +1,15 @@
 const ListService = {
   getList(knexInstance) {
     return knexInstance("list").select("*");
+  },
+  insertItem(knexInstance, newItem) {
+    return knexInstance
+      .insert(newItem)
+      .into("list")
+      .returning("*")
+      .then(rows => {
+        return rows[0];
+      });
   }
 };
 
